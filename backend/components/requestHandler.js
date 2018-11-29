34bad53bcec63fa322cd54f76311d86ceb72f1e7
sahithvibudhi <v.sahithkumar@gmail.com>
2018-11-29 23:29:56 +0530

    implementation of before and after request hook with just content-type

--- backend/components/requestHandler.js
+++ backend/components/requestHandler.js
@@ -1,12 +1,16 @@
 const qs            = require('querystring');
 const { jsonify }   = require('./input');
 const CRUD          = require('./CRUD');
+const beforeRequest = require('./../hooks/beforeReq');
+const afterRequest  = require('./../hooks/afterReq');
 
 module.exports = async (req, res) => {
     jsonify(req, res, handle)
 }
 
 async function handle(req, res) {
+    beforeRequest(req, res);
     await CRUD(req, res);
+    afterRequest(req, res);
     res.end();
 }
