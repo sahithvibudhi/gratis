91edf5515ac90aa8cde984da367b10d26afbbe31
sahithvibudhi <v.sahithkumar@gmail.com>
2018-12-23 15:00:03 +0530

    constant for NO_AUTH msg

--- backend/components/constants.js
+++ backend/components/constants.js
@@ -1,14 +1,19 @@
 module.exports = {
     
     RTYPE : {
-        GET     : 'GET',
-        POST    : 'POST',
-        PUT     : 'PUT',
-        DEL     : 'DELETE'
+        GET             : 'GET',
+        POST            : 'POST',
+        PUT             : 'PUT',
+        DEL             : 'DELETE'
     },
 
     CTYPE : {
         FORM_URLENCODED : 'application/x-www-form-urlencoded',
         JSON            : 'application/json',
+    },
+
+    MSGS : {
+        NO_AUTH         : 'You are not Authorised to perform this action'
     }
+    
 }
