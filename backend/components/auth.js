f045de735e4d17eaaffc021d4af71368f59da188
sahithvibudhi <v.sahithkumar@gmail.com>
2018-12-23 18:23:22 +0530

    Appending user details to request

--- backend/components/auth.js
+++ backend/components/auth.js
@@ -5,9 +5,13 @@ module.exports = {
 
     isGratisLoggedIn : async (req, res) => {
         if(req.headers.authorization){
-            var token = req.headers.Authorization;
-            var count = await db.getCollection({details:'/gratis/users'}).count({token});
-            return count > 0;
+            var token = req.headers.authorization;
+            var data = await db.getCollection({details:'/gratis/users'}).findOne({token});
+            // data found with the given token
+            if(data) {
+                req.userData = data;
+                return true;
+            }
         }
         return false;
     },
