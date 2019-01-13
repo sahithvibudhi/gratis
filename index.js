f95238ceb696b241551a1cf9ed8ce8d96c420bda
sahithvibudhi <v.sahithkumar@gmail.com>
2019-01-14 00:22:05 +0530

    .env optional for npm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 const cmdArgs     = require('minimist')(process.argv.slice(2));
-const envArgs     = require('dotenv').load().parsed;
+const envArgs     = require('dotenv').load().parsed || {};
 const { log }     = require('./backend/components/logger');
 
 global.argv       = Object.assign(envArgs, cmdArgs);
